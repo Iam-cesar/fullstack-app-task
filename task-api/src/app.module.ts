6964246452a07d7277c494c9afa17ad955c62e2d
Cesar Augusto <cesaraugustojun@gmail.com>
2025-02-23 14:29:57 -0300

    feat(task-api): update typeorm config

--- task-api/src/app.module.ts
+++ task-api/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './core/config/typeorm/typeOrmConfig';
+import { typeOrmConfig } from './core/config/typeorm/typeorm.config';
 import { TaskModule } from './task/task.module';
 
 @Module({
